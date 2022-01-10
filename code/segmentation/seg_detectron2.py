8f245ccdee3178ceff80674a6dbee60243de0276
Alicia Fortes Machado <afm@rhone.local>
2022-01-11 00:53:41 +0100

    Now we can use detectron2 to identify segmentation of first frame.

---
+++ code/segmentation/seg_detectron2.py
@@ -0,0 +1,88 @@
+"""
+Function that calls detectron2 to segmentate the first frame of a video.
+
+This part uses the structure of calling and doing the inference on the detectron2 from tutorial colab 
+in https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5
+from the official detectron2 repo.
+"""
+
+import numpy as np
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+
+def get_less_annotations(gd_annotations, new_annotations, threshold=0.7):
+  """
+  Returns the annotations that identify the same objects as the true
+  annotations.
+  Args:
+    gd_annotations: ground truth annotations on the data
+    new_annotations: new annotations using segmentation algorithm
+    threshold: float between 0 and 1
+  Returns:
+    less_annotations: new annotations but with less objects trying to mimic
+                      ground truth annotations
+  """
+  bool_gd = gd_annotations.astype(bool)
+  idxs_to_del = []
+
+  for i in range(new_annotations.shape[0]):
+    total = np.sum(new_annotations[i,:,:].astype(int))
+    intersection = np.sum(np.logical_and(bool_gd,
+                                      new_annotations[i,:,:]).astype(int))
+    if intersection / total < threshold:
+      idxs_to_del.append(i)
+  
+  less_annotations = np.delete(new_annotations, idxs_to_del, axis=0)
+
+  return less_annotations
+
+
+def get_arr_from_bools(annotations):
+    """
+    Functions that gets the array of integers representing the
+    masks from the annotations.
+    Args:
+        annotations: (N_objects, H, W) tensor containing annotations from segmentation
+                                        algorithm
+    Returns:
+        img_arr: (H, W) array of integers showing to which class each pixel is from
+    """
+    img_arr = np.zeros(annotations.shape[1:])
+    for i in range(len(annotations)-1, -1, -1):
+        img_arr[annotations[i].cpu().numpy()] = i + 1
+
+    return img_arr
+
+
+def get_mask_detectron2(orig_img, gd_annotations, threshold, 
+                                    limit_annotations):
+    """
+    Function that calls detectron2 to segmentate the first frame of a video.
+    Args:
+        orig_img: (H, W, 3)
+        gd_annotations: (H, W)
+        threshold: float
+        limit_annotations: bool
+    Returns:
+        auto_mask: (H, W)
+    """
+    # Load the model:
+    cfg = get_cfg()
+
+    # Add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    
+    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    
+    masks = predictor(orig_img)["instances"].pred_masks
+
+    if limit_annotations:
+        masks = get_less_annotations(gd_annotations, masks, threshold)
+    
+    auto_mask = get_arr_from_bools(masks)
+    return auto_mask
---
+++ code/segmentation/seg_first_frame.py
@@ -0,0 +1,102 @@
+"""
+File that calls the model of your preference to segmentate the first frame and
+that saves it into the output folder we want.
+"""
+
+from argparse import ArgumentParser
+import numpy as np
+import pickle
+from os import path
+import os
+from PIL import Image
+from seg_detectron2 import get_mask_detectron2
+import cv2
+
+"""
+Arguments loading
+"""
+parser = ArgumentParser()
+# TODO: take dataset into consideration
+# The idea here is to save the automatic first frame inside the folder of the dataset
+parser.add_argument('--real_path', default='../DAVIS/2017/trainval')
+parser.add_argument('--pred_path', default='../DAVIS/2017/trainval')
+parser.add_argument('--output_path', default='../metric_results')
+parser.add_argument('--seg_algo', options='swin-transformer / mask-r-cnn / detectron2', default='detectron2')
+parser.add_argument('--limit_annotations', options='True / False', default=True)
+parser.add_argument('--threshold', options='interval [0,1]', default=0.7)
+
+args = parser.parse_args()
+
+imgs_path = args.real_path
+pred_path = args.pred_path
+out_path = args.output_path
+seg_algo = args.seg_algo
+limit_annotations = args.limit_annotations
+threshold = args.threshold
+
+get_masks = {
+    'detectron2': get_mask_detectron2,
+}
+
+# TODO: discover where to save the first frames, so that it is easily accessible with the current code 
+# TODO: discover how to call the segmentation algorithms
+
+# We will save our annotations into a new folder called Auto_Annotations
+# List the directories in real path / ImageSets and create them in a new "Auto_Annotations" folder
+# Create folder if it doesn't exist yet
+pred_path = path.join(pred_path, 'Auto_Annotations')
+if not path.exists(pred_path):
+    os.makedirs(pred_path)
+    os.makedirs(path.join(pred_path, '480p'))
+
+pred_path = path.join(pred_path, '480p')
+anns_path = path.join(imgs_path, '480p', 'Annotations')
+
+# TODO: 480p or 1080p ???
+imgs_path = path.join(imgs_path, 'JPEGImages', '480p')
+vid_list = sorted(os.listdir(imgs_path))
+
+# TODO: temporary solution
+# Remove .DStore files in macbook
+for v in vid_list:
+    if v.startswith('.'):
+        vid_list.remove(v)
+
+print('Segmenting first frames...')
+
+# Do the prediction for each first frame
+get_palette = True
+
+for vid in vid_list:
+    # Find paths for the first frame and the annotations
+    pred_vid = path.join(pred_path, vid)
+
+    # Find where to save the annotations calculated by the chosen algorithm
+    img_path = path.join(imgs_path, vid)
+    ann_path = path.join(anns_path, vid)
+
+    if not path.exists(pred_vid):
+        os.makedirs(pred_vid)
+
+    # Read the frames from original image and true annotations
+    # We use the true annotations to identify the same annotations
+    # so that we can compare the results with manually
+    # annotated frames
+    gd_annotations = np.array(Image.open(ann_path).convert("P"))
+    orig_img = cv2.imread(img_path)
+
+    # We only need to take the palette once
+    if get_palette:
+        palette = palette = Image.open(path.expanduser(ann_path)).getpalette()
+        get_palette = False
+
+    # Do the prediction using the algorithm selected
+    masks_arr = get_masks[seg_algo](orig_img, gd_annotations, threshold=threshold, 
+                                    limit_annotations=limit_annotations)
+
+    # Save the first frame into the folder
+    mask = Image.fromarray(masks_arr).convert("P")
+    mask.putpalette(palette)
+    mask.save('test.png')
+
+print('Finished!')
