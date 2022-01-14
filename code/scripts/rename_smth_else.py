8f232d5cb449bc0d6cfe15e276da59f586da82cb
Alicia Fortes Machado <aliciafortesmachado@gmail.com>
2022-01-14 20:12:28 +0100

    Fixing rename_smth_else.py.

--- code/scripts/rename_smth_else.py
+++ code/scripts/rename_smth_else.py
@@ -11,6 +11,10 @@ parser.add_argument('--smth_else_path', default='../videos')
 
 args = parser.parse_args()
 
+def get_right_name(filename):
+    number = int(filename.split('.')[0])
+    return '{:05d}.jpg'.format(number - 1)
+
 if os.path.exists(args.smth_else_path):
     os.rename(args.smth_else_path, os.path.join(os.path.dirname(args.smth_else_path), 
             'something-something'))
@@ -25,4 +29,12 @@ filenames = [f for f in os.listdir(new_smth_else_path) if not f == 'tracking_ann
 
 for filename in filenames:
     os.rename(os.path.join(new_smth_else_path, filename), 
-              os.path.join(new_smth_else_path, 'videos', filename))
+            os.path.join(new_smth_else_path, 'videos', filename))
+    pth = os.path.join(new_smth_else_path, 'videos', filename)
+
+    if os.path.exists(os.path.join(pth, 'DONE')):
+        os.remove(os.path.join(pth, 'DONE'))
+
+    for file in os.listdir(pth):
+        os.rename(os.path.join(pth, file), 
+                  os.path.join(pth, get_right_name(file)))
